f512aa378d7a16c8e6bbae9df49f05d9facb2224
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-01 20:13:58 -0500

    working pawng (could add audio, font)

--- examples/SampleUnityProject/Assets/Scripts/PaddleScript.cs
+++ examples/SampleUnityProject/Assets/Scripts/PaddleScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PaddleScript : MonoBehaviour
 {
+    private float yPos;
+    public float paddleSpeed = 0.2f;
+    public float topWall, bottomWall;
+
+    public KeyCode upKey, downKey;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class PaddleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // moving down
+        if (Input.GetKey(downKey)) {
+            if (yPos > bottomWall) {
+                yPos -= paddleSpeed;
+            }
+        }
+
+        // moving up
+        if (Input.GetKey(upKey)) {
+            if (yPos < topWall) {
+                yPos += paddleSpeed;
+            }
+        }
+
+        transform.localPosition = new Vector3(transform.position.x, yPos, 0);
     }
 }
