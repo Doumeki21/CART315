3ded922150537ae4c77f48827786cb16338c117d
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-18 18:41:53 -0500

    need to fix gun and bullet connection with player

---
+++ Examples/SHMUP/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+    public Vector2 direction = new Vector2(1,0);  //moves the bullet to the right
+    public float bulletSpeed = 20f;
+    
+    public Vector2 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //destroy this game object (the bullet) after a number of seconds.
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = direction * bulletSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        pos += velocity * Time.fixedDeltaTime;
+        transform.position = pos;
+    }
+}
--- Examples/SHMUP/Assets/Scripts/PlayerController.cs
+++ Examples/SHMUP/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //an array of gun types recognized by the player.
+    private Gun[] guns;
+    
     private float yPos, xPos;
     public float playerSpeed = 1.0f;
     public float topWall, bottomWall, leftWall, rightWall;
 
     public KeyCode upKey, downKey, leftKey, rightKey;
+    
+    private bool shoot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Gets access to all the gun types in all the child game obj.
+        guns = transform.GetComponentsInChildren<Gun>();
     }
 
     // Update is called once per frame
@@ -42,7 +48,16 @@ public class PlayerController : MonoBehaviour
                 xPos += playerSpeed;
             }
         }
-
         transform.localPosition = new Vector3(xPos, yPos, 0);
+        
+        shoot = Input.GetKeyDown(KeyCode.P);
+        if (shoot)
+        {
+            // shoot = false;
+            foreach(Gun gun in guns)
+            {
+                gun.Shoot();
+            }
+        }
     }
 }
