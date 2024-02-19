170c6d1fe68cd3a17707fc054236277fa2df4e26
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-18 23:59:09 -0500

    bullets move across screen

--- Examples/SHMUP/Assets/Scripts/BulletScript.cs
+++ Examples/SHMUP/Assets/Scripts/BulletScript.cs
@@ -5,29 +5,13 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-
-    public Vector2 direction = new Vector2(1,0);  //moves the bullet to the right
-    public float bulletSpeed = 200f;
-    
-    public Vector2 velocity;
-    
-    // Start is called before the first frame update
+    public float speed = 20f;
+    public Rigidbody2D rb;
+        
+    // Use this for initialization
     void Start()
     {
-        //destroy this game object (the bullet) after a number of seconds.
-        Destroy(gameObject, 3);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        velocity = direction * bulletSpeed;
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 pos = transform.position;
-        pos += velocity * Time.fixedDeltaTime;
-        transform.position = pos;
+        //velocity is the current speed on the axes.
+        rb.velocity = transform.right * speed;
     }
 }
