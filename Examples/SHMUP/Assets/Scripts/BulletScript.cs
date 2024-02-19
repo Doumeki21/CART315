1a85789dba15600cd7fe949a2701237243ffce06
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-19 00:46:54 -0500

    created enemy placeholder + need to implement impact & death fx

--- Examples/SHMUP/Assets/Scripts/BulletScript.cs
+++ Examples/SHMUP/Assets/Scripts/BulletScript.cs
@@ -6,12 +6,27 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float speed = 20f;
+    public int damage = 40;
     public Rigidbody2D rb;
+    public GameObject impactEffect;
         
     // Use this for initialization
     void Start()
     {
         //velocity is the current speed on the axes.
         rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        //if we actually find an enemy component,
+        if (enemy != null)
+        {
+            //can input a damage value here or in the public.
+            enemy.TakeDamage(damage);
+        }
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
