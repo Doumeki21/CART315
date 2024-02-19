2fd7e31abaa3f2bcdcf3b45f88a37b297ff3da85
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-19 02:00:05 -0500

    removed beam code

--- Examples/SHMUP/Assets/Scripts/Weapon.cs
+++ Examples/SHMUP/Assets/Scripts/Weapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -7,7 +8,10 @@ public class Weapon : MonoBehaviour
     //Weapon code borrowed from Brackey's "2D shooting in Unity tutorial".
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public int beamDamage = 80;
+    
+    // public int beamDamage = 80;
+    // public GameObject impactBeamEffect;
+    // public LineRenderer lineRenderer;
 
     // Update is called once per frame
     void Update()
@@ -18,10 +22,10 @@ public class Weapon : MonoBehaviour
             Shoot();
         }
 
-        if (Input.GetButtonDown("Fire2"))
-        {
-            ShootBeam();
-        }
+        // if (Input.GetButtonDown("Fire2"))
+        // {
+        //     StartCoroutine(ShootBeam());
+        // }
     }
 
     void Shoot()
@@ -32,16 +36,34 @@ public class Weapon : MonoBehaviour
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 
-    void ShootBeam()
-    {
-       RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
-       if (hitInfo)
-       {
-           Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
-           if (enemy != null)
-           {
-               enemy.TakeDamage(beamDamage);
-           }
-       }
-    }
+    // IEnumerator ShootBeam()
+    // {
+    //    RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+    //    
+    //    if (hitInfo)
+    //    {
+    //        Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+    //        if (enemy != null)
+    //        {
+    //            enemy.TakeDamage(beamDamage);
+    //        }
+    //        Instantiate(impactBeamEffect, hitInfo.point, Quaternion.identity);
+    //        
+    //        LineRenderer.SetPosition(0, firePoint.position);
+    //        LineRenderer.SetPosition(1, hitInfo.point);
+    //    }
+    //    else
+    //    {
+    //        //if the line doesn't hit anything, it continues infinitely in space.
+    //        LineRenderer.SetPosition(0, firePoint.position);
+    //        LineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100); //take the starting position and shifting it 100 units fold.
+    //    }
+    //    
+    //    //to make this part work, this (void) function needs to be a coroutine
+    //    lineRenderer.enabled = true;
+    //    //wait 1 frame 
+    //    yield return 0;
+    //
+    //    lineRenderer.enabled = false;
+    // }
 }
