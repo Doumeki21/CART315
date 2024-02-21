9f224645b1ba61b5feb5a11aa60e1e523f730a1a
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-02-20 23:20:56 -0500

    YEY enemies can be unaffected offscreen!

--- Examples/SHMUP/Assets/Scripts/BulletScript.cs
+++ Examples/SHMUP/Assets/Scripts/BulletScript.cs
@@ -9,8 +9,6 @@ public class BulletScript : MonoBehaviour
     public int damage = 40;
     public Rigidbody2D rb;
     // public GameObject impactEffect;
-
-    public bool isEnemy = false;
         
     // Use this for initialization
     void Start()
@@ -22,10 +20,10 @@ public class BulletScript : MonoBehaviour
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Enemy enemy = hitInfo.GetComponent<Enemy>();
-        //if we actually find an enemy component,
-        if (enemy != null)
+        //if we actually find an enemy component + they're within boundaries,
+        if (enemy != null && enemy.transform.position.x < 730)
         {
-            //Take damage (can input a damage value here or in the public.)
+            //Enemies take damage (can input a damage value here or in the public.)
             enemy.TakeDamage(damage);
             // Instantiate(impactEffect, transform.position, transform.rotation);
             Destroy(gameObject);
