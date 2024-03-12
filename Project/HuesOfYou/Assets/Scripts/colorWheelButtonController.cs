a93349089e0ed94e84dc32e05f109bf47cb691c0
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2024-03-12 18:00:24 -0400

    yeee color wheel animation works on hover!!

--- Project/HuesOfYou/Assets/Scripts/colorWheelButtonController.cs
+++ Project/HuesOfYou/Assets/Scripts/colorWheelButtonController.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class colorWheelButtonController : MonoBehaviour
 {
+    public int ID;
+    private Animator anim;
+    private bool selected = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    public void Selected()
+    {
+        selected = true;
+    }
+    
+    public void Deselected()
+    {
+        selected = false;
+    }
+
+    public void HoverEnter()
+    {
+        anim.SetBool("Hover", true);
+    }
+    
+    public void HoverExit()
+    {
+        anim.SetBool("Hover", false);
     }
 }
